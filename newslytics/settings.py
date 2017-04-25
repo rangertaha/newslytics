a6a07b33dd22b2087b91da2be060286a58049ea4
rangertaha <tahasaidi@gmail.com>
2017-04-24 20:40:00 -0400

    updates

--- newslytics/settings.py
+++ newslytics/settings.py
@@ -88,10 +88,21 @@ WSGI_APPLICATION = 'newslytics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'newslytics',
+        'USER': 'coder',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
